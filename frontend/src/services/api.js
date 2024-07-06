f1ac5f83fd07e60132729b2ff97a038daf6393e1
Michael Jonsson <mjonsson1@gmail.com>
2024-07-06 01:13:00 -0400

    messed with api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL
+const API_BASE_URL = process.env.REACT_APP_API_URL || '';
 
 export const fetchNFTs = async () => {
     try {
