523ef8ed234343fe44b359aaeecf54688f3d7fbe
Michael Jonsson <mjonsson1@gmail.com>
2024-07-23 09:31:40 -0400

    pushing latest from mj

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
 
- const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
-//const API_BASE_URL = process.env.REACT_APP_API_URL || '';
+// const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+const API_BASE_URL = process.env.REACT_APP_API_URL || '';
 
 export const fetchNFTs = async () => {
     try {
